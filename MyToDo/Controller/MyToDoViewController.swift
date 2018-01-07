5a884bac520b4f223b51834b016f71e2b10676aa
Sai Pratap Koppolu <KSP@apples-macbook-pro.local>
2018-01-07 13:50:07 +0530

    Converted to MVC design Pattern

--- MyToDo/Controller/MyToDoViewController.swift
+++ MyToDo/Controller/MyToDoViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 
 class MyToDoViewController: UITableViewController {
     
-    var itemList = ["Eat","Sleep","Repeat"]
+    var itemList = [Item]()
     var userDefaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = userDefaults.array(forKey: "ToDoItemList") as? [String] {
+        if let items = userDefaults.array(forKey: "ToDoItemList") as? [Item] {
             itemList = items
         }
     }
@@ -29,7 +29,10 @@ class MyToDoViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemList[indexPath.row]
+        let item = itemList[indexPath.row]
+        cell.textLabel?.text = item.itemName
+        
+        cell.accessoryType = item.state ? .checkmark : .none
         return cell
     }
     
@@ -38,11 +41,8 @@ class MyToDoViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemList[indexPath.row].state = !itemList[indexPath.row].state
+        self.tableView.reloadData()
     }
     
     //MARK: - Add New Items
@@ -53,8 +53,12 @@ class MyToDoViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            self.itemList.append(textField.text!)
-            self.userDefaults.setValue(self.itemList, forKey: "ToDoItemList")
+            
+            let newItem = Item()
+            newItem.itemName = textField.text!
+            self.itemList.append(newItem)
+            
+            self.userDefaults.set(self.itemList, forKey: "ToDoItemList")
             self.tableView.reloadData()
         }
         
---
+++ MyToDo/New Group/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  MyToDo
+//
+//  Created by KoppoluSaiPratap on 07/01/18.
+//  Copyright © 2018 KoppoluSaiPratap. All rights reserved.
+//
+
+import UIKit
+
+class Item {
+    var itemName: String = ""
+    var state: Bool = false
+}
